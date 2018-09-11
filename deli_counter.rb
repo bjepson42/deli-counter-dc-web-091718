62351a2f24a6696e4487cfd90833558c2d1a2889
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-09-11 22:40:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,18 +1,13 @@
 # Write your code here.
 def line(currentLine)
-  if currentLine.length == 0
-    {puts "The line is currently empty."
-
-    } else {
+  #if currentLine.length == 0  puts "The line is currently empty."
   lineAnnouncement = "The line is currently:"
   i = 0
   while i < currentLine
     lineAnnouncement = lineAnnouncement + " " + i + 1 + ". " + currentLine[i]
     i =+ 1
-  }
   end
   return currentLine
-
 end
 
 def take_a_number(currentLine, newCustomer)
