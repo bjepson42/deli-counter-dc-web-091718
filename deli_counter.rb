7c0f47bd216b838ee931fd1211511a2a7b9c8ccc
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-09-11 22:44:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 def line(currentLine)
   if currentLine.length == 0 {
       puts "The line is currently empty."
-      return currentLine
+      #return currentLine
   }
   end
   lineAnnouncement = "The line is currently:"
