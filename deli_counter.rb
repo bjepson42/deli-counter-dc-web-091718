8ff0128ac0c9daadee12542f5860334a3481d2cf
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-09-11 22:02:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,17 @@
 # Write your code here.
 def line(currentLine)
+  lineAnnouncement = "The line is currently:"
+  i = 0
+  while i < currentLine
+    lineAnnouncement = lineAnnouncement + " " + i + ". " currentLine[i]
+    i =+ 1
+  end
   return currentLine
 end
 
 def take_a_number(currentLine, newCustomer)
   currentLine.push newCustomer
+  puts "Welcome, #{newCustomer}. You are number #{current + 1} in line."
   return currentLine
 end
 
