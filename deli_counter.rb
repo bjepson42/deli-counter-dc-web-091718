f1b654bdb9718d65cfd47e451d7d2b5611bbd0ee
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-09-11 22:41:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,9 @@
 # Write your code here.
 def line(currentLine)
-  #if currentLine.length == 0  puts "The line is currently empty."
+  if currentLine.length == 0
+    {puts "The line is currently empty."
+      return currentLine
+    }
   lineAnnouncement = "The line is currently:"
   i = 0
   while i < currentLine
